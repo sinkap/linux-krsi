cc83f08a5996ca025124a3e8e3153582f559d093
KP Singh <kpsingh@google.com>
2019-09-10 13:55:22 +0200

    bpf: lsm: Add a helper function bpf_lsm_event_output
    This helper is similar to bpf_perf_event_output except that
    it does need a ctx argument which is more usable in the
    BTF based LSM programs where the context is converted to
    the signature of the attacthed BTF type.
    
    An example usage of this function would be:
    
    struct {
             __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
             __uint(key_size, sizeof(int));
             __uint(value_size, sizeof(u32));
    } perf_map SEC(".maps");
    
    BPF_TRACE_1(bpf_prog1, "lsm/bprm_check_security,
    	    struct linux_binprm *, bprm)
    {
    	char buf[BUF_SIZE];
    	int len;
    	u64 flags = BPF_F_CURRENT_CPU;
    
    	/* some logic that fills up buf with len data */
    	len = fill_up_buf(buf);
    	if (len < 0)
    		return len;
    	if (len > BU)
    		return 0;
    
    	bpf_lsm_event_output(&perf_map, flags, buf, len);
    	return 0;
    }
    
    Signed-off-by: KP Singh <kpsingh@google.com>
    Change-Id: Id67ba3467ba5c442415baa6ac25fcc0da82f8daf

--- security/bpf/ops.c
+++ security/bpf/ops.c
@@ -8,7 +8,23 @@
 #include <linux/bpf.h>
 #include <linux/btf.h>
 
-const struct bpf_prog_ops lsm_prog_ops = {
+BPF_CALL_4(bpf_lsm_event_output,
+	   struct bpf_map *, map, u64, flags, void *, data, u64, size)
+{
+	if (unlikely(flags & ~(BPF_F_INDEX_MASK)))
+		return -EINVAL;
+
+	return bpf_event_output(map, flags, data, size, NULL, 0, NULL);
+}
+
+static const struct bpf_func_proto bpf_lsm_event_output_proto =  {
+	.func		= bpf_lsm_event_output,
+	.gpl_only       = true,
+	.ret_type       = RET_INTEGER,
+	.arg1_type      = ARG_CONST_MAP_PTR,
+	.arg2_type      = ARG_ANYTHING,
+	.arg3_type      = ARG_PTR_TO_MEM,
+	.arg4_type      = ARG_CONST_SIZE_OR_ZERO,
 };
 
 static const struct bpf_func_proto *get_bpf_func_proto(
@@ -19,10 +35,15 @@ static const struct bpf_func_proto *get_bpf_func_proto(
 		return &bpf_map_lookup_elem_proto;
 	case BPF_FUNC_get_current_pid_tgid:
 		return &bpf_get_current_pid_tgid_proto;
+	case BPF_FUNC_lsm_event_output:
+		return &bpf_lsm_event_output_proto;
 	default:
 		return NULL;
 	}
 }
+
+const struct bpf_prog_ops lsm_prog_ops = {
+};
 
 const struct bpf_verifier_ops lsm_verifier_ops = {
 	.get_func_proto = get_bpf_func_proto,
