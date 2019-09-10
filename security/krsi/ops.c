d77e68e867c73c1ddc516ba65ce5a142110d596d
KP Singh <kpsingh@google.com>
2019-09-10 13:55:15 +0200

    krsi: Introduce types for KRSI eBPF
    KRSI intrdocues a new eBPF program type BPF_PROG_TYPE_KRSI with an
    expected attach type of BPF_KRSI. An -EINVAL error is returned if an
    attachment is requested.
    
    Signed-off-by: KP Singh <kpsingh@google.com>
    Change-Id: I2c1ac8b2cd82007a0648b4edcb3c774b50eec0a8

---
+++ security/krsi/ops.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <linux/filter.h>
+#include <linux/bpf.h>
+
+const struct bpf_prog_ops krsi_prog_ops = {
+};
+
+const struct bpf_verifier_ops krsi_verifier_ops = {
+};
