b69d564502438e6dd7b93a63ed7c32a151d1f41c
KP Singh <kpsingh@google.com>
2019-09-10 13:55:19 +0200

    krsi: Implement eBPF operations, attachment and execution
    A user space program can attach an eBPF program by:
    
      hook_fd = open("/sys/kernel/security/krsi/process_execution", O_RDWR)
      prog_fd = bpf(BPF_PROG_LOAD, ...)
      bpf(BPF_PROG_ATTACH, hook_fd, prog_fd)
    
    When such an attach call is received, the attachment logic looks up the
    dentry and appends the program to the bpf_prog_array.
    
    The BPF programs are stored in a bpf_prog_array and writes to the array
    are guarded by a mutex. The eBPF programs are executed as a part of the
    LSM hook they are attached to. If any of the eBPF programs return
    an error (-ENOPERM) the action represented by the hook is denied.
    
    Signed-off-by: KP Singh <kpsingh@google.com>
    Change-Id: I956340085a2ff6a7e3e9064d816506aa2aa2006d

--- security/krsi/include/krsi_init.h
+++ security/krsi/include/krsi_init.h
@@ -5,12 +5,29 @@
 
 #include "krsi_fs.h"
 
+#include <linux/binfmts.h>
+
 enum krsi_hook_type {
 	PROCESS_EXECUTION,
 	__MAX_KRSI_HOOK_TYPE, /* delimiter */
 };
 
 extern int krsi_fs_initialized;
+
+struct krsi_bprm_ctx {
+	struct linux_binprm *bprm;
+};
+
+/*
+ * krsi_ctx is the context that is passed to all KRSI eBPF
+ * programs.
+ */
+struct krsi_ctx {
+	union {
+		struct krsi_bprm_ctx bprm_ctx;
+	};
+};
+
 /*
  * The LSM creates one file per hook.
  *
@@ -33,9 +50,43 @@ struct krsi_hook {
 	 * The dentry of the file created in securityfs.
 	 */
 	struct dentry *h_dentry;
+	/*
+	 * The mutex must be held when updating the progs attached to the hook.
+	 */
+	struct mutex mutex;
+	/*
+	 * The eBPF programs that are attached to this hook.
+	 */
+	struct bpf_prog_array __rcu	*progs;
 };
 
 extern struct krsi_hook krsi_hooks_list[];
+
+static inline int krsi_run_progs(enum krsi_hook_type t, struct krsi_ctx *ctx)
+{
+	struct bpf_prog_array_item *item;
+	struct bpf_prog *prog;
+	struct krsi_hook *h = &krsi_hooks_list[t];
+	int ret, retval = 0;
+
+	preempt_disable();
+	rcu_read_lock();
+
+	item = rcu_dereference(h->progs)->items;
+	while ((prog = READ_ONCE(item->prog))) {
+		ret = BPF_PROG_RUN(prog, ctx);
+		if (ret < 0) {
+			retval = ret;
+			goto out;
+		}
+		item++;
+	}
+
+out:
+	rcu_read_unlock();
+	preempt_enable();
+	return IS_ENABLED(CONFIG_SECURITY_KRSI_ENFORCE) ? retval : 0;
+}
 
 #define krsi_for_each_hook(hook) \
 	for ((hook) = &krsi_hooks_list[0]; \
