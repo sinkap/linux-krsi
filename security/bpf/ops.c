a1bec1e7729e3d721721eebfca8c7bdb9c987c6d
KP Singh <kpsingh@google.com>
2019-10-16 15:00:33 +0200

    bpf: lsm: Introduce types for eBPF based LSM
    A new eBPF program type BPF_PROG_TYPE_LSM with an
    expected attach type of BPF_LSM_MAC. An -EINVAL error is returned if an
    attachment is currently requested.
    
    Signed-off-by: KP Singh <kpsingh@google.com>
    Change-Id: I2c1ac8b2cd82007a0648b4edcb3c774b50eec0a8

---
+++ security/bpf/ops.c
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+ * Copyright 2019 Google LLC.
+ */
+
+#include <linux/filter.h>
+#include <linux/bpf.h>
+
+const struct bpf_prog_ops lsm_prog_ops = {
+};
+
+const struct bpf_verifier_ops lsm_verifier_ops = {
+};
