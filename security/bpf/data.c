da21448b91f625f007967fd61543420e4dd9712c
KP Singh <kpsingh@google.com>
2019-10-20 23:12:53 +0200

    prototype: Store information about the files executed by a process
    Store the information about the files that were executed by
    a proces.
    
    Change-Id: I1f98738b4f9da4bb42af1bd5ed4ad52b7b51c762

--- security/bpf/data.c
+++ security/bpf/data.c
@@ -10,6 +10,7 @@
 #include <linux/slab.h>
 #include <linux/spinlock.h>
 #include <linux/bpf.h>
+#include <linux/binfmts.h>
 
 #include "data.h"
 
@@ -22,8 +23,62 @@ static void bpf_lsm_task_to_inode(struct task_struct *t, struct inode *i)
 		pr_err("unable to determine pid in inode task hook\n");
 }
 
+static int bpf_lsm_bprm_set_creds(struct linux_binprm *bprm)
+{
+	struct bpf_lsm_task_blob *tsec;
+	struct inode *inode;
+	struct pid *pid;
+
+	tsec = get_bpf_lsm_task_blob(bprm->cred);
+	if (unlikely(!tsec))
+		return 0;
+
+	/*
+	 * Store the data of the executing process in the inode of the task.
+	 */
+	if (bprm->file && current) {
+		inode = file_inode(bprm->file);
+
+		pid = get_task_pid(current, PIDTYPE_PID);
+		if (!pid) {
+			// TODO: Why does this happen?
+			pr_err("unable to determine pid in proc exec hook\n");
+			return 0;
+		}
+		tsec->pid = pid;
+
+		spin_lock(&inode->i_lock);
+		__iget(inode);
+		spin_unlock(&inode->i_lock);
+		tsec->exec_inode = inode;
+	}
+	return 0;
+}
+
+static void bpf_lsm_task_free(struct task_struct *task)
+{
+	struct bpf_lsm_task_blob *tsec;
+
+	tsec = get_bpf_lsm_task_blob(task->cred);
+	if (unlikely(!tsec))
+		return;
+
+	/*
+	 * There are tasks like kernel threads which
+	 * won't have an associated inode in their creds as there is
+	 * no executable.
+	 */
+	if (!tsec->exec_inode) {
+		return;
+	}
+
+	iput(tsec->exec_inode);
+}
+
 static struct security_hook_list data_hooks[] __lsm_ro_after_init = {
 	LSM_HOOK_INIT(task_to_inode, bpf_lsm_task_to_inode),
+	LSM_HOOK_INIT(bprm_set_creds, bpf_lsm_bprm_set_creds),
+	LSM_HOOK_INIT(task_free, bpf_lsm_task_free),
 };
 
 struct lsm_blob_sizes bpf_lsm_blob_sizes __lsm_ro_after_init = {
