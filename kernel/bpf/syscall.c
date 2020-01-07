212605e53301b566ea5bfa8b9046bdb1eda5a1bf
KP Singh <kpsingh@google.com>
2020-01-07 23:45:14 +0100

    bpf: lsm: Introduce types for eBPF based LSM
    A new eBPF program type BPF_PROG_TYPE_LSM with an
    expected attach type of BPF_LSM_MAC. Attachment to LSM hooks is not
    implemented in this patch.
    
    On defining the types for the program, the macros expect that
    <prog_name>_prog_ops and <prog_name>_verifier_ops exist. This is
    implicitly required by the macro:
    
      BPF_PROG_TYPE(BPF_PROG_TYPE_LSM, lsm, ...)
    
    Signed-off-by: KP Singh <kpsingh@google.com>
    Reviewed-by: Brendan Jackman <jackmanb@google.com>
    Reviewed-by: Florent Revest <revest@google.com>
    Reviewed-by: Thomas Garnier <thgarnie@google.com>
    Change-Id: I2c1ac8b2cd82007a0648b4edcb3c774b50eec0a8

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -2396,6 +2396,9 @@ static int bpf_prog_attach(const union bpf_attr *attr)
 	case BPF_LIRC_MODE2:
 		ptype = BPF_PROG_TYPE_LIRC_MODE2;
 		break;
+	case BPF_LSM_MAC:
+		ptype = BPF_PROG_TYPE_LSM;
+		break;
 	case BPF_FLOW_DISSECTOR:
 		ptype = BPF_PROG_TYPE_FLOW_DISSECTOR;
 		break;
@@ -2426,6 +2429,9 @@ static int bpf_prog_attach(const union bpf_attr *attr)
 		break;
 	case BPF_PROG_TYPE_LIRC_MODE2:
 		ret = lirc_prog_attach(attr, prog);
+		break;
+	case BPF_PROG_TYPE_LSM:
+		ret = -EOPNOTSUPP;
 		break;
 	case BPF_PROG_TYPE_FLOW_DISSECTOR:
 		ret = skb_flow_dissector_bpf_prog_attach(attr, prog);
