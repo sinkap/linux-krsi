1d446c7ec5400f1900c1991d66fde0bea6c68e42
KP Singh <kpsingh@google.com>
2020-01-07 23:48:14 +0100

    bpf: lsm: Add a skeleton and config options
    The LSM can be enabled by CONFIG_SECURITY_BPF.
    Without CONFIG_SECURITY_BPF_ENFORCE, the LSM will run the
    attached eBPF programs but not enforce MAC policy based
    on the return value of the attached programs.
    
    Signed-off-by: KP Singh <kpsingh@google.com>
    Change-Id: I07d747b3ad20b4496e1a03efab0e86fb049bac97

---
+++ security/bpf/lsm.c
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+ * Copyright 2019 Google LLC.
+ */
+
+#include <linux/lsm_hooks.h>
+
+/* This is only for internal hooks, always statically shipped as part of the
+ * BPF LSM.
+ */
+static struct security_hook_list lsm_hooks[] __lsm_ro_after_init = {};
+
+static int __init lsm_init(void)
+{
+	security_add_hooks(lsm_hooks, ARRAY_SIZE(lsm_hooks), "bpf");
+	pr_info("eBPF and LSM are friends now.\n");
+	return 0;
+}
+
+DEFINE_LSM(bpf) = {
+	.name = "bpf",
+	.init = lsm_init,
+};
