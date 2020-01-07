78b3e48725f982826d6e3c3ec0d73ef46a47eed7
KP Singh <kpsingh@google.com>
2020-01-07 23:45:14 +0100

    bpf: lsm: Introduce types for eBPF based LSM
    A new eBPF program type BPF_PROG_TYPE_LSM with an
    expected attach type of BPF_LSM_MAC. Attachment to LSM hooks is not
    implemented in this patch.
    
    On defining the types for the program, the macros expect that
    <prog_name>_prog_ops and <prog_name>_verifier_ops exist. This is
    implicitly required by the macro:
    
      BPF_PROG_TYPE(BPF_PROG_TYPE_LSM, lsm, ...)
    
    Signed-off-by: KP Singh <kpsingh@google.com>
    Change-Id: I2c1ac8b2cd82007a0648b4edcb3c774b50eec0a8

--- include/linux/bpf_types.h
+++ include/linux/bpf_types.h
@@ -68,6 +68,10 @@ BPF_PROG_TYPE(BPF_PROG_TYPE_SK_REUSEPORT, sk_reuseport,
 #if defined(CONFIG_BPF_JIT)
 BPF_PROG_TYPE(BPF_PROG_TYPE_STRUCT_OPS, bpf_struct_ops,
 	      void *, void *)
+#ifdef CONFIG_SECURITY_BPF
+BPF_PROG_TYPE(BPF_PROG_TYPE_LSM, lsm,
+	       void *, void *)
+#endif /* CONFIG_SECURITY_BPF */
 #endif
 
 BPF_MAP_TYPE(BPF_MAP_TYPE_ARRAY, array_map_ops)
