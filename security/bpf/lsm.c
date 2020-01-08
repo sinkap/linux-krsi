c020bc521ec404666fc221d1620f1321d8a0bc72
KP Singh <kpsingh@google.com>
2020-01-09 00:09:21 +0100

    bpf: lsm: Add mutable hooks list for the BPF LSM
    - The list of hooks registered by an LSM is currently immutable as they
      are declared with __lsm_ro_after_init and they are attached to a
      security_hook_heads struct.
    - For the BPF LSM we need to de/register the hooks at runtime. Making
      the existing security_hook_heads mutable broadens an
      attack vector, so a separate security_hook_heads is added for only
      those that ~must~ be mutable.
    - These mutable hooks are run only after all the static hooks have
      successfully executed.
    
    This is based on the ideas discussed in:
    
      https://lore.kernel.org/lkml/20180408065916.GA2832@ircssh-2.c.rugged-nimbus-611.internal
    
    Change-Id: I88c48109cb54f9ee4afa15034922e8c555f87f2a
    Signed-off-by: KP Singh <kpsingh@google.com>

--- security/bpf/lsm.c
+++ security/bpf/lsm.c
@@ -4,6 +4,7 @@
  * Copyright 2019 Google LLC.
  */
 
+#include <linux/bpf_lsm.h>
 #include <linux/lsm_hooks.h>
 
 /* This is only for internal hooks, always statically shipped as part of the
@@ -11,6 +12,12 @@
  * which is common for LSMs and R/O after init.
  */
 static struct security_hook_list lsm_hooks[] __lsm_ro_after_init = {};
+
+/* Security hooks registered dynamically by the BPF LSM and must be accessed
+ * by holding bpf_lsm_srcu_read_lock and bpf_lsm_srcu_read_unlock. The mutable
+ * hooks dynamically allocated by the BPF LSM are appeneded here.
+ */
+struct security_hook_heads bpf_lsm_hook_heads;
 
 static int __init lsm_init(void)
 {
