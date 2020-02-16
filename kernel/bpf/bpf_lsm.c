bc142130dccf82d85018c34f4026e1f67bd9cacc
KP Singh <kpsingh@google.com>
2020-02-16 23:29:19 +0100

    bpf: lsm: Add support for enabling/disabling BPF hooks
    Each LSM hook defines a static key i.e. bpf_lsm_<name>
    and a bpf_lsm_<name>_set_enabled function to toggle the key
    which enables/disables the branch which executes the BPF programs
    attached to the LSM hook.
    
    Use of static keys was suggested in upstream discussion:
    
      https://lore.kernel.org/bpf/1cd10710-a81b-8f9b-696d-aa40b0a67225@iogearbox.net/
    
    and results in the following assembly:
    
      0x0000000000001e31 <+65>:    jmpq   0x1e36 <security_bprm_check+70>
      0x0000000000001e36 <+70>:    nopl   0x0(%rax,%rax,1)
      0x0000000000001e3b <+75>:    xor    %eax,%eax
      0x0000000000001e3d <+77>:    jmp    0x1e25 <security_bprm_check+53>
    
    which avoids an indirect branch and results in lower overhead which is
    especially helpful for LSM hooks in performance hotpaths.
    
    Given the ability to toggle the BPF trampolines, some hooks which do
    not call call_<int, void>_hooks as they have different default return
    values, also gain support for BPF program attachment.
    
    There are some hooks like security_setprocattr and security_getprocattr
    which are not instrumentable as they do not provide any monitoring or
    access control decisions. If required, generation of BTF type
    information for these hooks can be also be blacklisted.
    
    Change-Id: If800c785ca2510e9e88bd22f9acabab6b9969ec8
    Signed-off-by: KP Singh <kpsingh@google.com>
    Reviewed-by: Brendan Jackman <jackmanb@google.com>
    Reviewed-by: Florent Revest <revest@google.com>

--- kernel/bpf/bpf_lsm.c
+++ kernel/bpf/bpf_lsm.c
@@ -8,6 +8,20 @@
 #include <linux/bpf.h>
 #include <linux/btf.h>
 #include <linux/bpf_lsm.h>
+#include <linux/jump_label.h>
+#include <linux/kallsyms.h>
+
+#define LSM_HOOK(RET, NAME, ...)					\
+	DEFINE_STATIC_KEY_FALSE(bpf_lsm_key_##NAME);			\
+	void bpf_lsm_##NAME##_set_enabled(bool value)			\
+	{								\
+		if (value)						\
+			static_branch_enable(&bpf_lsm_key_##NAME);	\
+		else							\
+			static_branch_disable(&bpf_lsm_key_##NAME);	\
+	}
+#include <linux/lsm_hook_names.h>
+#undef LSM_HOOK
 
 /* For every LSM hook  that allows attachment of BPF programs, declare a NOP
  * function where a BPF program can be attached as an fexit trampoline.
@@ -22,6 +36,20 @@
 
 #include <linux/lsm_hook_names.h>
 #undef LSM_HOOK
+
+int bpf_lsm_set_enabled(const char *name, bool value)
+{
+	char toggle_fn_name[KSYM_NAME_LEN];
+	void (*toggle_fn)(bool value);
+
+	snprintf(toggle_fn_name, KSYM_NAME_LEN, "%s_set_enabled", name);
+	toggle_fn = (void *)kallsyms_lookup_name(toggle_fn_name);
+	if (!toggle_fn)
+		return -ESRCH;
+
+	toggle_fn(value);
+	return 0;
+}
 
 const struct bpf_prog_ops lsm_prog_ops = {
 };
