e0eda40d76f762d6fc106d734a7908a0cfcb2474
KP Singh <kpsingh@google.com>
2020-11-19 15:40:30 +0000

    bpf: Update LSM selftests for bpf_ima_inode_hash
    Update the IMA policy before executing the test binary,
    Call the bpf_ima_inode_hash in the bprm_committed_creds hook and check
    if the call succeeded and a hash was calculated.
    
    Change-Id: I6468292ffc2c6f8c309038e7b662137152494b81

--- tools/testing/selftests/bpf/prog_tests/test_lsm.c
+++ tools/testing/selftests/bpf/prog_tests/test_lsm.c
@@ -11,8 +11,10 @@
 #include <malloc.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <linux/limits.h>
 
 #include "lsm.skel.h"
+#include "trace_helpers.h"
 
 char *CMD_ARGS[] = {"true", NULL};
 
@@ -52,8 +54,27 @@ int exec_cmd(int *monitored_pid)
 	return -EINVAL;
 }
 
+#define IMA_POLICY "measure func=BPRM_CHECK"
+#define IMA_SYSFS_POLICY_FILE "/sys/kernel/security/ima/policy"
+
+static int update_ima_policy(void)
+{
+	int fd, ret = 0;
+
+	fd = open(IMA_SYSFS_POLICY_FILE, O_WRONLY);
+	if (fd < 0)
+		return -errno;
+
+	if (write(fd, IMA_POLICY, sizeof(IMA_POLICY)) == -1)
+		ret = -errno;
+
+	close(fd);
+	return ret;
+}
+
 void test_test_lsm(void)
 {
+	char ima_policy_backup[PATH_MAX] = "/tmp/ima_policy_bupXXXXXX";
 	struct lsm *skel = NULL;
 	int err, duration = 0;
 	int buf = 1234;
@@ -66,9 +87,24 @@ void test_test_lsm(void)
 	if (CHECK(err, "attach", "lsm attach failed: %d\n", err))
 		goto close_prog;
 
-	err = exec_cmd(&skel->bss->monitored_pid);
-	if (CHECK(err < 0, "exec_cmd", "err %d errno %d\n", err, errno))
+	err = copy_file_temp(IMA_SYSFS_POLICY_FILE, ima_policy_backup);
+	if (CHECK(err, "ima_policy_backup", "ima_policy_backup failed: %d\n",
+		  err))
 		goto close_prog;
+
+	err = update_ima_policy();
+	if (CHECK(err != 0, "update_ima_policy", "error = %d\n", err))
+		goto close_prog;
+
+	err = exec_cmd(&skel->bss->monitored_pid);
+	CHECK(err < 0, "exec_cmd", "err %d errno %d\n", err, errno);
+
+	err = copy_file(ima_policy_backup, IMA_SYSFS_POLICY_FILE);
+	CHECK(err, "ima_policy_restore", "ima_policy_restore err = %d\n", err);
+
+	err = unlink(ima_policy_backup);
+	CHECK(err, "cleanup ima backup", "unlink(%s) err = %d\n",
+	      ima_policy_backup, err);
 
 	CHECK(skel->bss->bprm_count != 1, "bprm_count", "bprm_count = %d\n",
 	      skel->bss->bprm_count);
@@ -82,6 +118,12 @@ void test_test_lsm(void)
 
 	CHECK(skel->bss->mprotect_count != 1, "mprotect_count",
 	      "mprotect_count = %d\n", skel->bss->mprotect_count);
+
+	CHECK(skel->data->ima_hash_ret < 0, "ima_hash_ret",
+	      "ima_hash_ret = %d\n", skel->data->ima_hash_ret);
+
+	CHECK(skel->bss->ima_hash == 0, "ima_hash",
+	      "ima_hash = %lu\n", skel->bss->ima_hash);
 
 	syscall(__NR_setdomainname, &buf, -2L);
 	syscall(__NR_setdomainname, 0, -3L);
